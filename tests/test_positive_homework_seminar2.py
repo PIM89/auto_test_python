3246cca6fc72d86919d7d900dc7e364dbffc3eac
PIM89 <z55666@yandex.ru>
2024-10-23 14:07:33 +0000

    change homework

--- tests/test_positive_homework_seminar2.py
+++ tests/test_positive_homework_seminar2.py
@@ -35,13 +35,10 @@ def test_7z_listing_arch():
     assert res_arch and res_view_file1 and res_view_file2, "test_7z_listing_arch fail"
 
 def test_7z_check_path():
-    res_arch = checkout_positive("cd {}; 7z a ../out/arx2".format(folder_in), "Everything is Ok")
     res_view_file2 = checkout_positive("cd {}; 7z l arx2.7z".format(folder_out), "folder/test2.txt")
-    assert res_arch and res_view_file2, "test_7z_check_path fail"
+    assert res_view_file2, "test_7z_check_path fail"
 
 def test_7z_hash():
-    res_arch = checkout_positive("cd {}; 7z a ../out/arx2".format(folder_in), "Everything is Ok")
     res_crc32 = subprocess.run("cd {}; crc32 arx2.7z".format(folder_out), shell=True, encoding="utf-8", stdout=subprocess.PIPE)
-    crc32_hash = res_crc32.stdout.upper()
-    res_compare_hash = checkout_positive("cd {}; 7z h arx2.7z".format(folder_out), crc32_hash)
-    assert res_arch and res_compare_hash, "test_7z_hash fail"
+    res_compare_hash = checkout_positive("cd {}; 7z h arx2.7z".format(folder_out), res_crc32.stdout.upper())
+    assert res_compare_hash, "test_7z_hash fail"
